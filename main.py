5102541c0c756d339a5ea99944bdb51491ec883a
aune150 <65126330+aune150@users.noreply.github.com>
2021-07-22 00:02:38 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import json
 from Formater_OLT_Data import formater
 from GarminConnectAPI import connect
 from OLT_API import olt
+import datetime
 
 
 class main:
-    def __init__(self, creds="creds.json", debug:bool=False) -> None:
+    def __init__(self, creds="C:/Users/aune1/NY/python/Connecttoolt/creds.json", debug:bool=False) -> None:
         self.debug = debug
         with open(creds) as f:
             creds = json.load(f)
-        self.GC = connect(creds["GarminConnect"]["usename"], creds["GarminConnect"]["password"], debug=debug)
+        self.GC = connect(creds["GarminConnect"]["username"], creds["GarminConnect"]["password"], debug=debug)
         self.O = olt(creds["OLT"]["username"], creds["OLT"]["password"], debug=debug)
         self.F = formater()
 
-    def get_økter(self, start=0, limit=1):
+    def get_økter(self, fra_dag, til_dag):
         "Lagrer økter i self.økt"
-        self.økt = self.GC.aktiv(start=start, limit=limit)
+        self.økt = self.GC.aktiv_per_dag(fra_dag, til_dag)
 
     def gå_igjennom_økter(self):
         "Går igjennom alle økter i self.økt og sender til OLT"
@@ -23,6 +24,11 @@ class main:
             self.GC.puls(i["activityId"])
             data = i
             puls = self.GC.hr_timezones
+
+            print("Dato: ", data["startTimeLocal"].split(" ")[0], "\n",
+            "Sted: ", data["activityName"], "\n",
+            "Distanse: ", data["distance"], "\n", 
+            "Varighet: ", datetime.timedelta(seconds=data["duration"]))
 
             navn = input("Navn på økten: ")
 
@@ -47,3 +53,8 @@ class main:
                 print(d)
 
             self.O.økt(self.F.økt(d))
+
+if __name__ == "__main__":
+    M = main()
+    M.get_økter(str(datetime.datetime.date(datetime.datetime.now())), str(datetime.datetime.date(datetime.datetime.now())))
+    M.gå_igjennom_økter()
